0687b2027c22fb951ab5fa694ca0ca187e0f4e75
ShahadAdil <71069861+ShahadAdil@users.noreply.github.com>
2023-03-23 11:21:06 +0100

    update &  NavBar

---
+++ Vue-Pro/src/components/NavBar.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+      <img class="navbar-brand" src="" />
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Pro</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <RouterLink class="nav-link active" aria-current="page" to="/"
+                >A</RouterLink
+              >
+            </li>
+
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/about">B</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+.offcanvas-title {
+  color: rgb(0, 0, 0);
+}
+
+.navbar-toggler {
+  background: rgb(255, 255, 255);
+}
+
+.navbar-brand {
+  height: 70px;
+  width: 70px;
+  border-radius: 50px;
+}
+</style>
--- Vue-Pro/src/main.js
+++ Vue-Pro/src/main.js
@@ -1,14 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+// import bootstrap to css and js
+import 'bootstrap/dist/css/bootstrap.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import './assets/base.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(bootstrap);
 
 app.mount('#app')
